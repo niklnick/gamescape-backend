2804dd1dae4317e08db7c9b61dca63fa32a069c4
neumann.niklas <neumann.niklas98@gmail.com>
2024-10-17 14:27:20 +0200

    role.enum & user.role

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,6 +2,7 @@ import { Exclude } from "class-transformer";
 import { Game } from "src/games/entities/game.entity";
 import { Material } from "src/materials/entities/material.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "../enums/role.enum";
 
 @Entity()
 export class User {
@@ -20,6 +21,10 @@ export class User {
     @Exclude()
     @Column()
     password: string;
+
+    @Exclude()
+    @Column('enum', { enum: Role, default: Role.User })
+    role: Role;
 
     @OneToMany(() => Game, (game: Game) => game.author)
     games: Game[];
