f49c9a1ace86b94888613bd27304cd5ceb3d52d9
neumann.niklas <neumann.niklas98@gmail.com>
2024-12-11 23:02:20 +0100

    typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities: true,
+            synchronize: true
+        })
+    ]
+})
 export class AppModule { }
